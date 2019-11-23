60ff611468d6f804aea213d47de1082f8be5d088
jasmintang <44978515+jasmintang@users.noreply.github.com>
2019-11-22 20:28:34 -0500

    Update Assignment_4.py

--- Assignment_4.py
+++ Assignment_4.py
@@ -28,27 +28,25 @@ print("RM average number of rooms per dwelling has highest effection")
 
 
 # K-means and the Wine data set
-
 from sklearn.datasets import load_wine
-import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 wine = load_wine()
+_total = []
+sse = {}
 for i in range(1,10):
     total = 0.0
     kmeans = KMeans(n_clusters=i, random_state=0).fit(wine.data)
     center = kmeans.cluster_centers_
-    for current_lable in range(i):
-        subtotal = 0
-        for j in range(len(center)):
-            for wine_index in range(len(wine.data)):
-                if kmeans.labels_[wine_index] == j and kmeans.labels_[wine_index] == current_lable:
-                    for k in range(len(center[j])):
-                        subtotal += (wine.data[wine_index][k] - center[j][k]) * (wine.data[wine_index][k] - center[j][k])
-        #print("lable ",current_lable," j ",j, " subtotal ",subtotal)
-        total+=subtotal
-    print(total)
-#print(kmeans.labels_)
-#print(kmeans.cluster_centers_)
+    sse[i] = kmeans.inertia_
+
+
+plt.figure()
+plt.plot(list(sse.keys()), list(sse.values()))
+plt.xlabel("Number of cluster")
+plt.ylabel("SSE")
+plt.show()
+
+print("number of clusters on the data is 2")
 
 
